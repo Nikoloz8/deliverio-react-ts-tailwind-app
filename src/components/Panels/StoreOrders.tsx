fc6ade3eea2ae1fa067ff20c403d475e0a583f30
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-07-20 21:17:15 +0400

    create orders render functionality in StoreOrders panel

--- src/components/Panels/StoreOrders.tsx
+++ src/components/Panels/StoreOrders.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react"
+
 export default function StoreOrders() {
+
+    const [orders, setOrders] = useState<any[]>([])
+    const [countPages, setCountPages] = useState(1)
+    const [pageOrders, setPageOrders] = useState<any[]>([])
+
+    const itemsPerPage = 6
+
+    useEffect(() => {
+        const stored = JSON.parse(localStorage.getItem("orders")!) || []
+        setOrders(stored)
+    }, [])
+
+    useEffect(() => {
+        const startIndex = (countPages - 1) * itemsPerPage
+        const newPage = orders.slice(startIndex, startIndex + itemsPerPage)
+        setPageOrders(newPage)
+    }, [countPages, orders])
+
+    const totalPages = Math.ceil(orders.length / itemsPerPage)
+
     return (
         <div className="p-[20px] rounded-[6px] bg-[#111111] mt-[24px]">
             <div className="flex justify-between items-center">
@@ -24,29 +46,31 @@ export default function StoreOrders() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="border-b-[1px] border-solid border-[#E0E6ED]">
-                        <td className="p-[13px_0_13px_40px] text-center font-[500] text-[1.4rem] leading-[100%] text-[#FFFFFF]">#00</td>
-                        <td className="text-center font-[300] text-[1.4rem] leading-[100%] text-[#FFFFFF]">Mar 05, 2025</td>
-                        <td className="text-center font-[300] text-[1.4rem] leading-[100%] text-[#FFFFFF]">QuickMart</td>
-                        <td className="text-center pr-[40px]">
-                            <span className="p-[3px_14px] bg-[rgba(0,171,85,0.3)] rounded-[35px] text-[#00AB55] leading-[100%] font-[500] text-[1.3rem]">
-                                Completed
-                            </span>
-                        </td>
-                    </tr>
+                    {pageOrders.map((order: any, index: number) => {
+                        return <tr key={index} className="border-b-[1px] border-solid border-[#E0E6ED]">
+                            <td className="p-[13px_0_13px_40px] text-center font-[500] text-[1.4rem] leading-[100%] text-[#FFFFFF]">#{index}</td>
+                            <td className="text-center font-[300] text-[1.4rem] leading-[100%] text-[#FFFFFF]">{order.თარიღი}</td>
+                            <td className="text-center font-[300] text-[1.4rem] leading-[100%] text-[#FFFFFF]">{order.მაღაზია}</td>
+                            <td className="text-center pr-[40px]">
+                                <span className={`p-[3px_14px] ${order.სტატუსი === "Completed" ? "bg-[rgba(0,171,85,0.3)] text-[#00AB55]" : order.სტატუსი === "In Process" ? "bg-[#292929] text-[#ACACAC]" : "bg-[rgba(255,0,0,0.3)] text-[#FF0000]"} rounded-[35px] leading-[100%] font-[500] text-[1.3rem]`}>
+                                    {order.სტატუსი}
+                                </span>
+                            </td>
+                        </tr>
+                    })}
                     <tr className="h-[21px]"></tr>
                 </tbody>
             </table>
             <div className="flex gap-[6px]">
-                <button className="p-[8px_16px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#292929] rounded-[8px]">
+                <button onClick={() => setCountPages((prev) => Math.max(prev - 1, 1))} className="p-[8px_16px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#292929] rounded-[8px]">
                     <img src="/images/Home/Polygon 1 (1).svg" className="rotate-[90deg] mt-[1px]" alt="" />
                     Prev
                 </button>
-                <button className="p-[8px_16px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#343434] rounded-[8px]">
+                <button onClick={() => setCountPages((prev) => prev < totalPages ? prev + 1 : prev)} className="p-[8px_16px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#343434] rounded-[8px]">
                     Next
                     <img src="/images/Home/Polygon 1 (1).svg" className="rotate-[-90deg] mt-[1px]" alt="" />
                 </button>
             </div>
 
-        </div>)
+        </div >)
 }
