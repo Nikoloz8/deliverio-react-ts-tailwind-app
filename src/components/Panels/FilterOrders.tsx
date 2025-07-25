439cba34089b2a6782af0c1d4e1b568ed3412966
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-07-25 15:21:25 +0400

    improve MyOrders panel design by dynamic routing

---
+++ src/components/Panels/FilterOrders.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import { useOutletContext } from "react-router-dom"
+
+export default function FilterOrders() {
+
+    const [showFilters, setShowFilters] = useState(false)
+    const { filterStore, setFilterStore, storesArr } = useOutletContext<TPanelsLayoutOutletContext>()
+
+    return (
+        <>
+            <div className={`absolute transition-all duration-500 max-w-[200px] w-[200px] left-[-210px] p-[14px] bg-[#292929] border-[0.8px] border-solid border-[#585858] rounded-[8px] flex flex-col gap-[12px] shadow-[0_0_4px_0_#00000040] ${!showFilters && "opacity-0"}`}>
+                <h5 className="font-[300] pl-[20px] text-[1.4rem] leading-[100%] text-[#ADADAD]">Store</h5>
+                {storesArr.map((e, i) => {
+                    return <h5 key={i} onClick={() => filterStore.includes(e) ? setFilterStore(filterStore.filter((el: string) => el !== e)) : setFilterStore([...filterStore, e])} className={`font-[300] cursor-pointer text-[1.4rem] flex gap-[6px] leading-[100%] text-[#FFFFFF] ${!filterStore.includes(e) && "pl-[20px]"}`}>
+                        {filterStore.includes(e) && <img src="/images/Home/Group 40674.svg" />}
+                        {e}
+                    </h5>
+                })}
+            </div>
+            <button onClick={() => setShowFilters(!showFilters)} className="p-[8px_20px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#343434] rounded-[8px]">
+                Filters
+                <img src="/images/Home/mynaui_filter.svg" alt="" />
+            </button>
+        </>
+    )
+}
--- src/components/Panels/StoreOrders.tsx
+++ src/components/Panels/StoreOrders.tsx
@@ -1,36 +1,21 @@
-import { useState } from "react"
-import { useNavigate, useOutletContext } from "react-router-dom"
+import { useLocation, useNavigate, useOutletContext } from "react-router-dom"
 import NextPrevButtons from "./NextPrevButtons"
 import SortOrders from "./SortOrders"
+import FilterOrders from "./FilterOrders"
 
 export default function StoreOrders() {
 
-    const { orders, storesArr, filterStore, setFilterStore, pageOrders, filteredOrders } = useOutletContext<TPanelsLayoutOutletContext>()
+    const { orders, pageOrders, filteredOrders } = useOutletContext<TPanelsLayoutOutletContext>()
 
     const navigate = useNavigate()
-
-
-
-    const [showFilters, setShowFilters] = useState(false)
+    const location = useLocation().pathname
 
     return (
         <div className="p-[20px] rounded-[6px] bg-[#111111] mt-[24px]">
             <div className="flex justify-between items-center">
-                <h1 className="text-[1.8rem] font-[500] leading-[100%] text-[#FFFFFF] ">Store Orders</h1>
+                <h1 className="text-[1.8rem] font-[500] leading-[100%] text-[#FFFFFF] ">{location.includes("store") ? "My Orders" : "Store Orders"}</h1>
                 <div className="flex gap-[10px] relative">
-                    <div className={`absolute transition-all duration-500 max-w-[200px] w-[200px] left-[-210px] p-[14px] bg-[#292929] border-[0.8px] border-solid border-[#585858] rounded-[8px] flex flex-col gap-[12px] shadow-[0_0_4px_0_#00000040] ${!showFilters && "opacity-0"}`}>
-                        <h5 className="font-[300] pl-[20px] text-[1.4rem] leading-[100%] text-[#ADADAD]">Store</h5>
-                        {storesArr.map((e, i) => {
-                            return <h5 key={i} onClick={() => filterStore.includes(e) ? setFilterStore(filterStore.filter((el: string) => el !== e)) : setFilterStore([...filterStore, e])} className={`font-[300] cursor-pointer text-[1.4rem] flex gap-[6px] leading-[100%] text-[#FFFFFF] ${!filterStore.includes(e) && "pl-[20px]"}`}>
-                                {filterStore.includes(e) && <img src="/images/Home/Group 40674.svg" />}
-                                {e}
-                            </h5>
-                        })}
-                    </div>
-                    <button onClick={() => setShowFilters(!showFilters)} className="p-[8px_20px] text-[1.2rem] leading-[100%] text-[#FFFFFF] font-[300] flex items-center cursor-pointer gap-[4px] bg-[#343434] rounded-[8px]">
-                        Filters
-                        <img src="/images/Home/mynaui_filter.svg" alt="" />
-                    </button>
+                    {location.includes("admin") && <FilterOrders />}
                     <SortOrders />
                 </div>
             </div>
