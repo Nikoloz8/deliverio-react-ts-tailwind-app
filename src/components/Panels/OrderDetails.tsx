8b92197e43adc8048f6bd8b8ebc2f1489b9ea2f1
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-07-27 23:17:33 +0400

    create order details edit functionality in OrderDetails panel

--- src/components/Panels/OrderDetails.tsx
+++ src/components/Panels/OrderDetails.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate, useOutletContext, useParams } from "react-router-dom"
 import StatusTab from "./StatusTab"
+import { useForm } from "react-hook-form"
 
 export default function OrderDetails() {
 
@@ -12,6 +13,7 @@ export default function OrderDetails() {
     const [showDelete, setShowDelete] = useState(false)
     const [showChangeStatus, setShowChangeStatus] = useState(false)
     const [showForm, setShowForm] = useState(false)
+    const [status, setStatus] = useState(order.სტატუსი)
 
     const navigate = useNavigate()
     const { setOrders } = useOutletContext<TPanelsLayoutOutletContext>()
@@ -24,12 +26,26 @@ export default function OrderDetails() {
         setShowDelete(!showDelete)
     }
 
-    const handleChangeStatus = (status: string) => {
-        const changedOrder = { ...order, სტატუსი: status }
+    const { register, watch, reset } = useForm({})
+
+    console.log(watch())
+
+    useEffect(() => {
+        reset({
+            Buyer: order.მყიდველი,
+            Store: order.მაღაზია,
+            Address: order.მისამართი,
+            Number: order["ტელეფონის ნომერი"],
+            Amount: order["რაოდენობა * ფასი"]
+        })
+    }, [])
+
+    const handleSaveForm = (status: string) => {
+        const changedOrder = { ...order, მყიდველი: watch().Buyer, მაღაზია: watch().Store, მისამართი: watch().Address, "ტელეფონის ნომერი": watch().Number, "რაოდენობა * ფასი": watch().Amount, სტატუსი: status }
         const newOrders = orders.map((e: any) => e === order ? changedOrder : e)
         setOrders(newOrders)
         localStorage.setItem("orders", JSON.stringify(newOrders))
-        setShowChangeStatus(false)
+        setShowForm(false)
     }
 
     return (
@@ -45,29 +61,35 @@ export default function OrderDetails() {
                     </h4>
                     <div className="flex flex-col gap-[8px]">
                         <label htmlFor="buyer" className="font-[275] text-[#FFFFFF] text-[1.4rem] leading-[100%]">Buyer</label>
-                        <input type="text" id="buyer" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
+                        <input {...register("Buyer")} type="text" id="buyer" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
                     </div>
                     <div className="flex flex-col gap-[8px]">
-                        <label htmlFor="store" className="font-[275] cursor-pointer text-[#FFFFFF] text-[1.4rem] leading-[100%]">Store</label>
-                        <input type="text" id="store" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
+                        <label htmlFor="store" className="font-[275] text-[#FFFFFF] text-[1.4rem] leading-[100%]">Store</label>
+                        <input {...register("Store")} type="text" id="store" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
                     </div>
                     <div className="flex flex-col gap-[8px]">
-                        <label htmlFor="address" className="font-[275] cursor-pointer text-[#FFFFFF] text-[1.4rem] leading-[100%]">Address</label>
-                        <input type="text" id="address" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
+                        <label htmlFor="address" className="font-[275] text-[#FFFFFF] text-[1.4rem] leading-[100%]">Address</label>
+                        <input {...register("Address")} type="text" id="address" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
                     </div>
                     <div className="flex flex-col gap-[8px]">
-                        <label htmlFor="number" className="font-[275] cursor-pointer text-[#FFFFFF] text-[1.4rem] leading-[100%]">Phone Number</label>
-                        <input type="text" id="number" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
+                        <label htmlFor="number" className="font-[275] text-[#FFFFFF] text-[1.4rem] leading-[100%]">Phone Number</label>
+                        <input {...register("Number")} type="text" id="number" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
                     </div>
                     <div className="flex flex-col gap-[8px]">
-                        <label htmlFor="amount" className="font-[275] cursor-pointer text-[#FFFFFF] text-[1.4rem] leading-[100%]">Amount</label>
-                        <input type="text" id="amount" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
+                        <label htmlFor="amount" className="font-[275] text-[#FFFFFF] text-[1.4rem] leading-[100%]">Amount</label>
+                        <input {...register("Amount")} type="text" id="amount" className="w-[350px] font-[275] text-[#FFFFFF] text-[1.2rem] leading-[100%] bg-[#343434] rounded-[4px] outline-none p-[5px_12px]" />
                     </div>
                     <div className="flex flex-col gap-[8px]">
                         <label className="font-[275] cursor-pointer text-[#FFFFFF] text-[1.4rem] leading-[100%]">Status</label>
                         <span onClick={() => setShowChangeStatus(!showChangeStatus)}>
-                            <StatusTab order={order} />
+                            <StatusTab order={order} status={status} />
                         </span>
+                    </div>
+                    <div className="flex justify-center">
+                        <div className="flex items-center gap-[6px]">
+                            <button onClick={() => handleSaveForm(status)} className="bg-[#585858] rounded-[6px] p-[8px_32px] font-[300] text-[#FFFFFF] text-[1.4rem] leading-[100%] cursor-pointer">Save</button>
+                            <button onClick={() => setShowForm(false)} className="bg-transparent rounded-[6px] p-[8px_32px] font-[300] text-[#FFFFFF] text-[1.4rem] leading-[100%] cursor-pointer">Cancel</button>
+                        </div>
                     </div>
                 </form>
                 <div className={`transition-all duration-1000 p-[24px] bg-[#111111] border-[0.8px] border-solid border-[#363636] rounded-[8px] right-[-540px] w-[324px] hidden flex-col gap-[16px] shadow-[0_0_4px_0_#00000040] items-start ${showChangeStatus && "flex!"}`}>
@@ -75,8 +97,8 @@ export default function OrderDetails() {
                         Change Order Status
                     </h3>
                     <div className="flex flex-col gap-[8px] items-start">
-                        <button onClick={() => handleChangeStatus("Completed")} className={`font-[300] p-[8px_24px] bg-[#0C3F25] cursor-pointer text-[#00AB55] rounded-[28px] text-[1.2rem] leading-[100%]`}>Delivered</button>
-                        <button onClick={() => handleChangeStatus("In Process")} className={`font-[300] p-[8px_24px] bg-[#292929] cursor-pointer text-[#FFFFFF] rounded-[30px] text-[1.2rem] leading-[100%]`}>In Process</button>
+                        <button onClick={() => setStatus("Completed")} className={`font-[300] p-[8px_24px] bg-[#0C3F25] cursor-pointer text-[#00AB55] rounded-[28px] text-[1.2rem] leading-[100%]`}>Delivered</button>
+                        <button onClick={() => setStatus("In Process")} className={`font-[300] p-[8px_24px] bg-[#292929] cursor-pointer text-[#FFFFFF] rounded-[30px] text-[1.2rem] leading-[100%]`}>In Process</button>
                         <button onClick={() => setShowChangeStatus(false)} className={`font-[300] p-[8px_24px] bg-[#580C0C] cursor-pointer text-[#FF0000] rounded-[30px] text-[1.2rem] leading-[100%]`}>Cancel</button>
                     </div>
                 </div>
--- src/components/Panels/StatusTab.tsx
+++ src/components/Panels/StatusTab.tsx
@@ -1,5 +1,8 @@
-export default function StatusTab({ order }: { order: any }) {
+export default function StatusTab({ order, status = "" }: { order: any, status?: String }) {
+
+    const realStatus = (status ? status : order.სტატუსი)
+
     return (
-        <button className={`font-[500] p-[10px_32px] ${order.სტატუსი === "Completed" ? "bg-[#0C3F25] cursor-pointer text-[#00AB55]" : order.სტატუსი === "In Process" ? "bg-[#292929] text-[#FFFFFF]" : order.სტატუსი === "Cancelled" ? "text-[#FF0000] bg-[#580C0C]" : ""} rounded-[35px] text-[1.3rem] leading-[100%] cursor-pointer`}>{order.სტატუსი === "Completed" ? "Delivered" : order.სტატუსი}</button>
+        <button className={`font-[500] p-[10px_32px] ${realStatus === "Completed" ? "bg-[#0C3F25] text-[#00AB55]" : realStatus === "In Process" ? "bg-[#292929] text-[#FFFFFF]" : realStatus === "Cancelled" ? "text-[#FF0000] bg-[#580C0C]" : ""} rounded-[35px] text-[1.3rem] leading-[100%] cursor-pointer`}>{realStatus === "Completed" ? "Delivered" : realStatus}</button>
     )
 }
